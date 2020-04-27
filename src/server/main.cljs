22443fa8b6802cdeaac3997abec1ddc4b8703bad
George Pierce <gpierce3@hotmail.com>
2020-04-27 09:48:01 -0400

    Added a listener.

--- src/server/main.cljs
+++ src/server/main.cljs
@@ -1,24 +1,33 @@
 (ns server.main
   ; Import NPM modules directly 
   ; https://shadow-cljs.github.io/docs/UsersGuide.html#npm
-  (:require ["crypto" :as crypto]))
+  (:require ["crypto" :as crypto])
+  (:require ["http" :as http]))
 
 ; This is standard ClojureScript code and the output should be run using Node.
 ;
 ; More details at https://shadow-cljs.github.io/docs/UsersGuide.html#target-node-script
 
-(def value-a 1)
+(defn request-handler [req res]
+      (.end res "foo"))
 
-(defonce value-b 2)
-
-(defn do-work []
-  (println (-> (crypto/randomBytes 16) (.toString "base64"))))
+; a place to hang onto the server so we can stop/start it
+(defonce server-ref
+         (volatile! nil))
 
 (defn reload! []
-  (println "Code updated.")
-  (println "Trying values:" value-a value-b)
-  (do-work))
+  (println "Code updated."))
+
 
 (defn -main []
-  (println "App loaded!")
-  (do-work))
+      (js/console.log "starting server")
+      (let [server (http/createServer #(request-handler %1 %2))]
+
+           (.listen server 3000
+                           (fn [err]
+                               (if err
+                                                 (js/console.error "server start failed")
+                                                 (js/console.info "http server running"))))
+
+
+           (vreset! server-ref server)))
